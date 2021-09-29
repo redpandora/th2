0e0cda66569f93d25578ff9fd54cdcfe75c743e1
John Pereira <John.P@debitsuccess.com>
2021-09-30 12:55:59 +1300

    Started product service

--- WeApplication1.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WeApplication1.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ WeApplication1.Infrastructure/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using WebApplication1.Data;
+
+namespace WeApplication1.Infrastructure
+{
+    public interface IProductService
+    {
+        Task<Product> Find(Guid productId);
+        Task<Product> FindByName(string productName);
+        Task<Guid> StoreProduct(Product product);
+        Task UpdateProduct(Product product);
+    }
+}
