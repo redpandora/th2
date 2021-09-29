58144c0f16e6fbc57a9eee57df3e880405d58721
John Pereira <John.P@debitsuccess.com>
2021-09-30 12:47:16 +1300

    Adding Data and Infrastucture projects
    Added Product definition

---
+++ WebApplication1.Data/Entities/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApplication1.Data
+{
+    /// <summary>
+    /// The product data in the database
+    /// </summary>
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+    }
+}
