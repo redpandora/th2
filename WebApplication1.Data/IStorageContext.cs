06797cc674f37403b23adc5182cf5a420d345c90
John Pereira <John.P@debitsuccess.com>
2021-09-30 14:47:22 +1300

    Finished Implementation of Storage context

---
+++ WebApplication1.Data/IStorageContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    /// <summary>
+    /// The database context for storage
+    /// </summary>
+    public interface IStorageContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ WebApplication1.Data/StorageContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    /// <summary>
+    /// The database context implementation
+    /// </summary>
+    public class StorageContext : DbContext, IStorageContext
+    {
+        public StorageContext(DbContextOptions<StorageContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
