9e0bf7687e3fb50f6e26d069efb485e7b609d05a
John Pereira <John.P@debitsuccess.com>
2021-09-30 15:03:34 +1300

    Added the API controller

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeApplication1.Infrastructure;
+using WeApplication1.Infrastructure.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ProductData))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetProduct([FromRoute] Guid id)
+        {
+            try
+            {
+                // validate arguments
+                if (id == Guid.Empty) throw new ArgumentException($"Argument {nameof(id)} not specified");
+
+                var product = await productService.Find(id);
+                if (product == null) return NotFound();
+                return Ok(product);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message ?? "Invalid parameters");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
